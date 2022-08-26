4a483c7293bfed52680383f1496b05b447cf4e6b
diasporx <diasporx@gmail.com>
2022-08-26 09:23:22 +0300

    ..

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
 
-    
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    if ($('header').length || $('.sidebar-left').length) {
+        let h = $('header');
+        let sl = $('.sidebar-left');
+        let hh = h.height()
+        sl.css('top', 'calc(' + hh + 'px + 20px)') // calc(hh + 20px);
+    }
 
 });
